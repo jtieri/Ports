5adc522f0791410a62d7decf3bc627e672d5b98c
jtreebeard <justin@thetieris.com>
2018-08-27 18:46:58 -0500

    Updated scan design.
    Scan now connects to the target host socket via an instance of InetSocketAddress. Connection timeout time is added also.

--- src/main/java/com/portz/Scan.java
+++ src/main/java/com/portz/Scan.java
@@ -1,9 +1,12 @@
 package com.portz;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 
 public class Scan {
+    private final int TIMEOUT = 200; // the timeout value to be used in milliseconds.
+
     private final ScanSettings scanSettings;
 
     private Scan(ScanSettings scanSettings) {
@@ -17,14 +20,14 @@ public class Scan {
     public void scanPorts() {
         for(int i = scanSettings.getStartPort(); i <= scanSettings.getEndPort(); i++) {
             try {
-                Socket sock = new Socket(scanSettings.getHost(), i);
+                Socket sock = new Socket();
+                sock.connect(new InetSocketAddress(scanSettings.getHost(), i), TIMEOUT);
+                sock.close();
 
                 System.out.println("Port: " + i + " is open.");
-                sock.close();
             } catch(IOException e) {
                 System.out.println("Port: " + i + " is closed.");
             }
         }
     }
-
 }
