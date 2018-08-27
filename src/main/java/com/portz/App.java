523c592976835aa44e9091e5d633d0bc8e49cbe5
jtreebeard <justin@thetieris.com>
2018-08-27 18:15:05 -0500

    Entry-point into the application.
    includes the main method.

---
+++ src/main/java/com/portz/App.java
@@ -0,0 +1,10 @@
+package com.portz;
+
+/**
+ * Entry point into the application. Includes the main method which builds and runs the application.
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
